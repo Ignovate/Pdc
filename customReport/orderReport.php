b83fb5f8dda2c2ec952b782a21d53fc982390da5
Suryanand <suryanand000@gmail.com>
2019-02-26 17:25:35 +0530

    order Report Update

--- customReport/orderReport.php
+++ customReport/orderReport.php
@@ -1,9 +1,9 @@
 <?php
 //include database configuration file
-$dbHost     = 'localhost';
-$dbUsername = 'ignov6p5_dev';
-$dbPassword = 'iGnovate007';
-$dbName     = 'ignov6p5_b2b';
+$dbHost     = 'pdcdrugstoreSQL';
+$dbUsername = 'root';
+$dbPassword = 'iwRCKICX4olDry4yQclM1x';
+$dbName     = 'pdc';
 
 //Create connection and select DB
 $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
@@ -13,7 +13,7 @@ if($db->connect_error){
 }else{
 
 //get records from database
-$query = $db->query("select t1.increment_id as ordernumber,_t3.name as pharmacyname,t2.value as firstname,_t2.value as lastname,_t3.name as approved_by,t1.status,t1.created_at as purchased_date,t1.updated_at as processed_date,t1.updated_at as delivered_date,SUM(_t1.qty_ordered) as orderedqty,SUM(_t1.qty_invoiced) as invoicedqty,SUM(_t1.qty_shipped) as shippedqty,SUM(_t1.qty_canceled) as canceledqty,t1.base_grand_total as total  from sales_flat_order `t1` left join sales_flat_order_item `_t1` on t1.entity_id = _t1.order_id left join customer_entity_varchar `t2` on t1.customer_id = t2.entity_id and t2.attribute_id = 5 left join customer_entity_varchar `_t2` on t1.customer_id = t2.entity_id and t2.attribute_id = 5 left join customer_entity_int `t3` on t1.customer_id = t3.entity_id and t3.attribute_id = 139 left join sales_flat_order_item `t4` on t1.entity_id = t4.order_id inner join retailstores `_t3` on t3.value = _t3.id where 1=1 group by _t1.order_id");
+$query = $db->query("select t1.increment_id as ordernumber, _t3.name as pharmacyname, t2.value as firstname, _t2.value as lastname, _t3.name as approved_by, t1.status, t1.created_at as purchased_date, t1.updated_at as processed_date, t1.updated_at as delivered_date, _t1.qty_ordered as orderedqty, _t1.qty_invoiced as invoicedqty, _t1.qty_shipped as shippedqty, _t1.qty_canceled as canceledqty, t1.base_grand_total as total from sales_flat_order `t1` left join sales_flat_order_item `_t1` on t1.entity_id = _t1.order_id left join customer_entity_varchar `t2` on t1.customer_id = t2.entity_id and t2.attribute_id = 5 left join customer_entity_varchar `_t2` on t1.customer_id = t2.entity_id and t2.attribute_id = 5 left join customer_entity_int `t3` on t1.customer_id = t3.entity_id and t3.attribute_id = 139 left join sales_flat_order_item `t4` on t1.entity_id = t4.order_id inner join retailstores `_t3` on t3.value = _t3.id where 1=1 group by t1.entity_id order by t1.entity_id DESC");
 
 if($query->num_rows > 0){
     $delimiter = ",";
