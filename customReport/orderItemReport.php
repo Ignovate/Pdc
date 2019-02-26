b477194de771be32a0b403020faf752fdb610322
Suryanand <suryanand000@gmail.com>
2019-02-26 19:48:15 +0530

    order Item Report
    Updated with new query and csv format

--- customReport/orderItemReport.php
+++ customReport/orderItemReport.php
@@ -1,9 +1,9 @@
 <?php
 //include database configuration file
-$dbHost     = 'localhost';
-$dbUsername = 'ignov6p5_dev';
-$dbPassword = 'iGnovate007';
-$dbName     = 'ignov6p5_b2b';
+$dbHost     = 'pdcdrugstoreSQL';
+$dbUsername = 'root';
+$dbPassword = 'iwRCKICX4olDry4yQclM1x';
+$dbName     = 'pdc';
 
 //Create connection and select DB
 $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
@@ -13,7 +13,7 @@ if($db->connect_error){
 }else{
 
 //get records from database
-$query = $db->query("select t1.increment_id as ordernumber,t1.status as status,t2.sku,t2.name,t2.price,t2.qty_ordered as ordered_qty,t2.qty_invoiced as invoiced_qty,t2.qty_shipped as shipped_qty,t2.qty_canceled as canceled_qty from sales_flat_order `t1` left join sales_flat_order_item `t2` on t1.entity_id = t2.order_id where 1=1");
+$query = $db->query("select t1.increment_id as ordernumber, t1.customer_email as email, t1.customer_firstname as firstname, t1.customer_lastname as lastname, t1.created_at as purchased_date, t1.updated_at as processed_date, t1.updated_at as delivered_date, t1.status as status, t2.sku, t2.name, t2.price, t2.qty_ordered as ordered_qty, t2.qty_invoiced as invoiced_qty, t2.qty_shipped as shipped_qty, t2.qty_canceled as canceled_qty from sales_flat_order `t1` left join sales_flat_order_item `t2` on t1.entity_id = t2.order_id where 1=1 order by t1.entity_id DESC");
 
 if($query->num_rows > 0){
     $delimiter = ",";
@@ -23,13 +23,13 @@ if($query->num_rows > 0){
     $f = fopen('php://memory', 'w');
     
     //set column headers
-    $fields = array('ordernumber', 'status', 'sku', 'name', 'price', 'ordered_qty', 'invoiced_qty', 'shipped_qty', 'canceled_qty');
+    $fields = array('ordernumber', 'email', 'firstname', 'lastname', 'purchased_date', 'processed_date', 'delivered_date', 'status', 'sku', 'name', 'price', 'ordered_qty', 'invoiced_qty', 'shipped_qty', 'canceled_qty');
     fputcsv($f, $fields, $delimiter);
     
     //output each row of the data, format line as csv and write to file pointer
     while($row = $query->fetch_assoc()){
         //$status = ($row['status'] == '1')?'Active':'Inactive';
-        $lineData = array($row['ordernumber'], $row['status'], $row['sku'], $row['name'], $row['price'], $row['ordered_qty'], $row['invoiced_qty'], $row['shipped_qty'], $row['canceled_qty']);
+        $lineData = array($row['ordernumber'], $row['email'], $row['firstname'], $row['lastname'], $row['purchased_date'], $row['processed_date'], $row['delivered_date'], $row['status'], $row['sku'], $row['name'], $row['price'], $row['ordered_qty'], $row['invoiced_qty'], $row['shipped_qty'], $row['canceled_qty']);
         fputcsv($f, $lineData, $delimiter);
     }
     
