0e9304b22bb15f05ab1aa1d2d775358e4580cb96
Suryanand <suryanand000@gmail.com>
2019-03-11 11:44:10 +0530

    Bulk OrderImport Renaming

--- app/code/local/Dever/Sales/controllers/Adminhtml/BulkController.php
+++ app/code/local/Dever/Sales/controllers/Adminhtml/BulkController.php
@@ -21,14 +21,18 @@ class Dever_Sales_Adminhtml_BulkController extends Mage_Adminhtml_Controller_Act
                 $uploader = new Varien_File_Uploader('filename');
                 // Any extention would work
                 $uploader->setAllowedExtensions(array('xlsx'));
-                $uploader->setAllowRenameFiles(false);
+                $uploader->setAllowRenameFiles(true);
                 $uploader->setFilesDispersion(false);
                 // We set media as the upload dir
                 $path = Mage::getBaseDir('var') . DS . 'import';
+                
+                $UploadFileName = $_FILES['filename']['name'];
+                
                 $uploader->save($path, $_FILES['filename']['name'] );
                 $fileName = str_replace(" ","_",$_FILES['filename']['name']);
                 //this way the name is saved in DB
                 $data['filename'] = $fileName;
+                
             } else {
                 if(isset($data['filename']['delete']) && $data['filename']['delete'] == 1) {
                     $data['filename'] = '';
@@ -39,8 +43,10 @@ class Dever_Sales_Adminhtml_BulkController extends Mage_Adminhtml_Controller_Act
             //$data['updated_at'] = date ('Y-m-d H:i:s');
             $bulk->addData($data)->save();
             $this->_getSession()->addSuccess(
-                $this->__("Sheet uploaded successfully")
+                $this->__("Orders Sheet uploaded successfully")
             );
+            $newfilename = "ordersnew.xlsx";
+            rename($path."/".$fileName,$path."/".$newfilename);
         } catch (Exception $e) {
             $this->_getSession()->addError($e->getMessage());
         }
