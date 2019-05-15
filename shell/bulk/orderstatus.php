8a295c68f53f001eec0b801ce5c8b56458750653
Suryanand <suryanand000@gmail.com>
2019-05-15 13:36:50 +0530

    test

--- shell/bulk/orderstatus.php
+++ shell/bulk/orderstatus.php
@@ -163,15 +163,21 @@ class Dever_Shell_Bulk_Orders extends Mage_Shell_Abstract
                     $canceled[] = $item->getQtyCanceled();
                 }
             if( array_sum($ordered) == array_sum($shipped) + array_sum($canceled)){
-				echo "State Before : "; 
-				print_r($ordersplit->getState());
-				echo ";\n";
-                if($ordersplit->setStatus('canceled')
-                            ->save()){
-				echo "State After : ";
-				print_r($ordersplit->getState());
-				echo ";\n";
-							}
+					echo "State Before : "; 
+					print_r($ordersplit->getState());
+					echo ";\n";
+					echo "Status Before : "; 
+					print_r($ordersplit->getStatus());
+					echo ";\n";
+                $ordersplit->setStatus('canceled')
+                            ->save();
+					echo "State After : ";
+					print_r($ordersplit->getState());
+					echo ";\n";
+					echo "Status After : "; 
+					print_r($ordersplit->getStatus());
+					echo ";\n";
+				}
             }
         }
     }
