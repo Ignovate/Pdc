c129863d2079b718c9810c9dc9d8f6296008baeb
Suryanand <suryanand000@gmail.com>
2019-05-15 13:07:13 +0530

    Update Order Status

--- shell/bulk/orderstatus.php
+++ shell/bulk/orderstatus.php
@@ -162,7 +162,7 @@ class Dever_Shell_Bulk_Orders extends Mage_Shell_Abstract
                     $canceled[] = $item->getQtyCanceled();
                 }
             if( array_sum($ordered) == array_sum($shipped) + array_sum($canceled)){
-                $ordersplit->setStatus('Partially Complete')
+                $ordersplit->setStatus('Partially Completed')
                             ->save();
 				echo "Inside SetStatus as PC";
             }
