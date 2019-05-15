89cc4a212442a0f4391193d58cbc36b08289ace1
Suryanand <suryanand000@gmail.com>
2019-05-15 16:25:20 +0530

    Update

--- shell/bulk/orderstatus.php
+++ shell/bulk/orderstatus.php
@@ -169,7 +169,7 @@ class Dever_Shell_Bulk_Orders extends Mage_Shell_Abstract
 					echo "Status Before : "; 
 					print_r($ordersplit->getStatus());
 					echo ";\n";
-				$ordersplit->setState(Mage_Sales_Model_Order::STATE_COMPLETE, true)
+				$ordersplit->setState('partially_completed')
 							->save(); 	
 					echo "State After : ";
 					print_r($ordersplit->getState());
